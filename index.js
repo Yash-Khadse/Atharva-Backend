9690aacb93c218d6aebf46b849a5460062fbeb78
Yash Khadse <144233903+Yash-Khadse@users.noreply.github.com>
2024-04-16 12:14:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const UserSchema = new mongoose.Schema({
 // Create the User model
 const User = mongoose.model("User", UserSchema);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7887;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
